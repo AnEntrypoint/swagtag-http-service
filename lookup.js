2a9fbc2e5a92b2860cb77294dfa16e05732c1f65
lanmower <almagestfraternite@gmail.com>
2022-01-31 14:33:29 +0000

    cache fixes and validation

--- lookup.js
+++ lookup.js
@@ -6,6 +6,91 @@ const ABI = [
   {
     "inputs": [
       {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      }
+    ],
+    "name": "balanceOf",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "index",
+        "type": "uint256"
+      }
+    ],
+    "name": "tokenURI",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "owner",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "index",
+        "type": "uint256"
+      }
+    ],
+    "name": "tokenOfOwnerByIndex",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_tokenId",
+        "type": "uint256"
+      }
+    ],
+    "name": "getAddressForId",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function",
+    "constant": true
+  },
+  {
+    "inputs": [
+      {
         "internalType": "string",
         "name": "_name",
         "type": "string"
@@ -22,27 +107,55 @@ const ABI = [
     "stateMutability": "view",
     "type": "function",
     "constant": true
-  },
+  }
 ];
 
-module.exports = async (outname, question, net, contract, prefix, any) => {
+module.exports = async (outname, question, net, contract, prefix, any, invalidate) => {
   if (!net) return;
   if (!nets[net]) {
     nets[net] = [];
     const web3 = nets[net].web3 = new Web3(net);
     nets[net].contract = new web3.eth.Contract(ABI, contract);
-    nets[net].cache = new NodeCache({ stdTTL: 60 * 60 * 10, checkperiod: 120 });
+    nets[net].cache = new NodeCache({ stdTTL: 300, checkperiod: 30 });
   }
-
   const update = async () => {
-    let address = await nets[net].contract.methods.getAddress(outname.toLowerCase()).call();
+    console.log(outname);
+    if(outname.startsWith('0x')) {
+      if(invalidate) value = nets[net].cache.del( outname );
+      else {
+        const cached = nets[net].cache.get(outname);
+        if(cached) return cached;
+      }
+      let balance = await nets[net].contract.methods.balanceOf(outname.toLowerCase()).call();
+      console.log({balance, net, contract});
+      const out = [];
+      for(let index = 0; index < balance; index++) {
+        let tokenIndex = await nets[net].contract.methods.tokenOfOwnerByIndex(outname, index).call();
+        let address = await nets[net].contract.methods.getAddressForId(tokenIndex).call();
+        try {
+          address = JSON.parse(address);
+          address.token_id = tokenIndex;
+          out.push(address);
+        } catch(e) {
+          console.log(e);
+          out.push({token_id:tokenIndex, uri:await nets[net].contract.methods.tokenURI(tokenIndex).call()});
+        }
+      }
+      nets[net].cache.set(outname, out);
+      return out;
+    }
+    console.log({net});
+    const cache = nets[net].cache.get(question);
+    if (cache) return cache;
+    let address = nets[net].cache.get(outname.toLowerCase());
+    if(!address) address = await nets[net].contract.methods.getAddress(outname.toLowerCase()).call();
+    nets[net].cache.set(outname.toLowerCase(), address);
     try {
       address = JSON.parse(address);
+      nets[net].cache.set(outname, address);
     } catch (e) { }
     if (address && any) return address;
-    return address || '{}';
+    return address || {};
   };
-  const cache = nets[net].cache.get(question);
-  if (cache) return cache;
   return update() || '';
 };
--- server.js
+++ server.js
@@ -5,6 +5,57 @@ const DHT = require("@hyperswarm/dht");
 const net = require("net");
 const axios = require('axios');
 require('dotenv').config();
+
+const validateSubdomain = (subdomain) => {
+  const MIN_LENGTH = 1;
+  const MAX_LENGTH = 63;
+  const ALPHA_NUMERIC_REGEX = /^[a-z][a-z-]*[a-z0-9]*$/;
+  const START_END_HYPHEN_REGEX = /A[^-].*[^-]z/i;
+  const reservedNames = [
+    "www",
+    "ftp",
+    "mail",
+    "pop",
+    "smtp",
+    "admin",
+    "ssl",
+    "sftp"
+  ];
+  //if is reserved...
+  if (reservedNames.includes(subdomain))
+    throw new Error("cannot be a reserved name");
+
+  //if is too small or too big...
+  if (subdomain.length < MIN_LENGTH || subdomain.length > MAX_LENGTH)
+    throw new Error(
+      `must have between ${MIN_LENGTH} and ${MAX_LENGTH} characters`
+    );
+
+  //if subdomain is started/ended with hyphen or is not alpha numeric
+  if (!ALPHA_NUMERIC_REGEX.test(subdomain) || START_END_HYPHEN_REGEX.test(subdomain))
+    throw new Error(
+      subdomain.indexOf("-") === 0 ||
+      subdomain.indexOf("-") === subdomain.length - 1
+        ? "cannot start or end with a hyphen"
+        : "must be alphanumeric (or hyphen)"
+    );
+
+  return true;
+};
+
+const nets = {
+  'fuji': {
+    host: "https://api.avax-test.network/ext/bc/C/rpc",
+    prefix: 'https://domains.fujiavax.ga/',
+    contract: "0x171344b5C75a6F20b7db20F6F1d1Ab039C0bf85F",
+  },
+  'avax': {
+    host: "https://api.avax.network/ext/bc/C/rpc",
+    prefix: 'https://domains.avax.ga/',
+    contract: "0xD78b1732aBc4A25C17824Ea9510D7a66a4a09A75",
+  }
+}
+
 const agent = new http.Agent(
   {
     maxSockets: Number.MAX_VALUE,
@@ -15,40 +66,84 @@ const agent = new http.Agent(
 );
 const b32 = require("hi-base32")
 const fs = require('fs');
-const getListings = async (serverUrl, appId, netHost) => {
+const getListings = async (serverUrl, appId, netHost, host) => {
   const Moralis = require("moralis/node");
   Moralis.initialize(appId);
   Moralis.serverURL = serverUrl;
   Moralis.start({ appId, serverUrl });
+  const doneTrades = [];
+  const Trades = Moralis.Object.extend("Trades");
+  const query = new Moralis.Query(Trades);
+  query.descending("createdAt");
+  const results = await query.find();
+  const outTrades = [];
+  for (let i = 0; i < results.length; i++) {
+    const object = results[i];
+    const name = object.get('name').replace(host.prefix, '');
+    try {
+      if(doneTrades[name]) continue;
+      doneTrades.push(name);
+      validateSubdomain(name);
+      const output = Object.assign({},await lookup(name, name, host.host,host.contract, host.prefix));
+      output.uri = object.get('name');
+      output.token_id = object.get('item');
+      output.trade_id = object.get('ad');
+      output.price = object.get('price');
+      outTrades.push(output)
+    } catch(e) {
+      console.log(e);
+    }
+  }
+  for (x of Object.keys(nets)) {
+    if (nets[x].host === netHost) nets[x].trades = outTrades;
+  }
+
   Moralis.Cloud.run("List", JSON.stringify({})).then(
     async listings => {
+      console.log('listings');
       const out = [];
+      const done = [];
       for (listing of listings) {
-        response = await axios.get(listing.address.image);
-        console.log(listing.address.image)
-        try {
-          await axios.get(listing.address.image);
-          try {
-            await axios.get('https://'+listing.address.uri);
-          } catch(e) {
-            listing.disableLink = true;
-          }
-          out.push(listing);
-          console.log({ listing });
+        if(done.includes(listing.name)) {
+          continue
+        } else {
+          done.push(listing.name);
+        }
+        /*try {
+          lookup = lookup.join('/');
+          console.log('checking', lookup);
+          await axios.get(lookup+'/', {timeout:1000});
+          console.log('passed');
         } catch (e) {
-          console.error(e)
+          console.error('failed ', lookup);
+          try {
+            lookup = lookup.replace('https', 'http');
+            console.log('checking', lookup);
+            await axios.get(lookup+'/', {timeout:1000});
+            listing.enableHttp = true;
+          } catch(e) {
+          }
+          listing.disableLink = true;
+        }*/
+        try {
+          //console.log('checking image', listing.address.image);
+          await axios.get(listing.address.image);
+          out.push(listing);
+        } catch (e) {
+          console.error('failed image', lookup)
+          console.error('failed');
         }
       }
       for (x of Object.keys(nets)) {
         if (nets[x].host === netHost) nets[x].listings = out;
       }
-
+      console.log('done');
     }
   );
 }
 const getAllListings = async () => {
-  await getListings(process.env.REACT_APP_MORALIS_TESTNET_SERVER_URL, process.env.REACT_APP_MORALIS_TESTNET_APPLICATION_ID, 'https://api.avax-test.network/ext/bc/C/rpc');
-  //await getListings(process.env.REACT_APP_MORALIS_SERVER_URL, process.env.REACT_APP_MORALIS_APPLICATION_ID, 'https://api.avax.network/ext/bc/C/rpc');
+  await getListings(process.env.REACT_APP_MORALIS_TESTNET_SERVER_URL, process.env.REACT_APP_MORALIS_TESTNET_APPLICATION_ID, 'https://api.avax-test.network/ext/bc/C/rpc', nets['fuji']);
+  await getListings(process.env.REACT_APP_MORALIS_SERVER_URL, process.env.REACT_APP_MORALIS_APPLICATION_ID, 'https://api.avax.network/ext/bc/C/rpc', nets['avax']);
 }
 setInterval(getAllListings, 60000);
 getAllListings();
@@ -73,31 +168,21 @@ const closeOther = (local, other) => {
   local.on('end', () => { other.end() })
 }
 
-const getKey = async (name, question, contract, host, prefix) => {
+const getKey = async (name, question, host, contract, prefix) => {
   let publicKey;
   let decoded = '';
+  if(name === 'www') return Buffer.from(b32.decode.asBytes('4eytcu72vmojkgkjxvgpkpl76qg5gh23bxca7flwqpxovtkrbhdq'.toUpperCase()))
   try { decoded = b32.decode.asBytes(name.toUpperCase()) } catch (e) { }
+  
   if (decoded.length == 32) {
     publicKey = Buffer.from(decoded);
   } else {
-    const lookupRes = await lookup(name, question, contract, host, prefix) || '';
+    const lookupRes = await lookup(name, question, host, contract, prefix) || '';
     if (lookupRes.tunnel) publicKey = Buffer.from(b32.decode.asBytes(lookupRes.tunnel.toUpperCase()));
   }
   return publicKey;
 }
 
-const nets = {
-  'fuji': {
-    host: "https://api.avax-test.network/ext/bc/C/rpc",
-    prefix: 'https://domains.fujiavax.ga/',
-    contract: "0x171344b5C75a6F20b7db20F6F1d1Ab039C0bf85F",
-  },
-  'avax': {
-    host: "https://api.avax.network/ext/bc/C/rpc",
-    prefix: 'https://domains.avax.ga/',
-    contract: "0xc290698f5E5CdbF881d804f68ceb5b76Ada383Be",
-  }
-}
 const doServer = async function (req, res) {
   if (!req.headers.host) return;
   const split = req.headers.host.split('.');
@@ -112,8 +197,7 @@ const doServer = async function (req, res) {
   }
   split.splice(1, 2);
   let name = split.join('.')
-  if(name === 'fujiavax') name = 'www';
-  console.log({name});
+  if (name === 'fujiavax') name = 'www';
   if (name.length != 32) {
     if (name === 'bumps') {
       res.writeHead(200, {
@@ -122,6 +206,36 @@ const doServer = async function (req, res) {
       });
       console.log(host);
       res.end(JSON.stringify(host.listings));
+    }
+    if (name === 'trades') {
+      res.writeHead(200, {
+        'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin': '*'
+      });
+      console.log(host);
+      res.end(JSON.stringify(host.trades));
+    }
+    if (name === 'balance') {
+      let lookupRes = false;
+      try {
+        const paramsplit = req.url.split('?');
+        lookupRes = await lookup(req.url.split('/')[1].split('?')[0], req.headers.host, host.host, host.contract, host.prefix, true, (paramsplit.length>1)?paramsplit[1]:null);
+      } catch (e) { console.error(e) }
+      if (lookupRes) {
+        res.writeHead(200, {
+          'Content-Type': 'text/plain',
+          'Access-Control-Allow-Origin': '*'
+        });
+        console.log('looking up balance');
+        res.end(JSON.stringify(lookupRes));
+      } else {
+        res.writeHead(404, {
+          'Content-Type': 'text/plain',
+          'Access-Control-Allow-Origin': '*'
+        });
+        res.end(JSON.stringify([]));
+      }
+      return;
     }
     if (name === 'domains') {
       let lookupRes = false;
@@ -217,9 +331,9 @@ sserver.addContext('avax.ga', {
   cert: fs.readFileSync('plaincert.pem')
 });
 const doUpgrade = async function (req, socket, head) {
+  console.log(req.headers);
   const split = req.headers.host.split('.');
-  let host = nets['avax'];
-
+  let host = nets['fuji'];
   if (split.length > 2 && Object.keys(nets).includes(split[split.length - 3])) {
     host = nets[split[split.length - 3]];
     split.pop();
@@ -227,9 +341,9 @@ const doUpgrade = async function (req, socket, head) {
   if (split.length > 1 && Object.keys(nets).includes(split[split.length - 2])) {
     host = nets[split[split.length - 2]];
   }
-
   split.splice(1, 2);
   let name = split.join('.')
+  if (name === 'fujiavax') name = 'www';
 
   const publicKey = await getKey(name, req.headers.host, host.host, host.contract, host.prefix);
   proxy.ws(req, socket, {
